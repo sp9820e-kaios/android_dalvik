10e94f340e3e9d6d3f043396f4a02ecf6efbc384
yonglong <yonglong_liu@xunrui.com.cn>
2024-07-13 11:31:59 +0000

    Add spreadtrum changes

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2006 The Android Open Source Project
+# Copyright (C) 2009 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,50 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 LOCAL_PATH := $(call my-dir)
 
-subdirs := $(addprefix $(LOCAL_PATH)/,$(addsuffix /Android.mk, \
-		libdex \
-		dexgen \
-		dexlist \
-		dexdump \
-		dx \
-		tools \
-	))
+# Fake libdvm
 
-include $(subdirs)
+include $(CLEAR_VARS)
 
+LOCAL_SRC_FILES := fake_dvm.cpp
+LOCAL_C_INCLUDES := $(JNI_H_INCLUDE)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libdvm
 
-.PHONY: dex dex-debug
-ifeq ($(DONT_INSTALL_DEX_FILES),true)
-dex:
-	@echo "Forcing a remake with DONT_INSTALL_DEX_FILES=false"
-	$(hide) $(MAKE) DONT_INSTALL_DEX_FILES=false
-else
-# DONT_INSTALL_DEX_FILES is already false, so a normal make takes care of it.
-dex: $(DEFAULT_GOAL)
-endif
-
-d :=
-ifneq ($(GENERATE_DEX_DEBUG),)
-d := debug
-endif
-ifneq ($(DONT_INSTALL_DEX_FILES),true)
-d := $(d)-install
-endif
-ifneq ($(d),debug-install)
-# generate the debug .dex files, with a copy in ./dalvik/DEBUG-FILES.
-# We need to rebuild the .dex files for the debug output to be generated.
-# The "touch -c $(DX)" is a hack that we know will force
-# a rebuild of the .dex files.  If $(DX) doesn't exist yet,
-# we won't touch it (-c) and the normal build will create
-# the .dex files naturally.
-dex-debug:
-	@echo "Forcing an app rebuild with GENERATE_DEX_DEBUG=true"
-	@touch -c $(DX)
-	$(hide) $(MAKE) DONT_INSTALL_DEX_FILES=false GENERATE_DEX_DEBUG=true
-else
-# GENERATE_DEX_DEBUG and DONT_INSTALL_DEX_FILES are already set properly,
-# so a normal make takes care of it.
-dex-debug: $(DEFAULT_GOAL)
-endif
+include $(BUILD_SHARED_LIBRARY)
